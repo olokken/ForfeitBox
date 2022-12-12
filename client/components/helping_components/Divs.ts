194db696e5dd173bb36e00cab0b61a19bdb16884
MathiasMyrold <55196479+MathiasMyrold@users.noreply.github.com>
2022-12-12 15:36:13 +0100

    Menu card (#18)
    * Added interfaces, repos and services to program.cs
    
    * Adding dep injections
    
    * Made menuCard component

---
+++ client/components/helping_components/Divs.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const CenterContnetCol = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CenterContnetRow = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 5px;
+`;
+
+export const FlexWithGap = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 30px;
+`;
