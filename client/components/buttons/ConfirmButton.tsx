8be118ac3cf5d2255e6f0f76624f2e357d4277c3
Ole Løkken <55095305+olokken@users.noreply.github.com>
2022-12-25 17:31:45 +0100

    Navbar (#20)
    * added navbar

--- client/components/buttons/ConfirmButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ConfirmButton() {
-  return (
-    <div>ConfirmButton</div>
-  )
-}
-
-export default ConfirmButton
--- client/components/inputfields/Searchbar.tsx
+++ client/components/inputfields/Searchbar.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { GoSearch } from "react-icons/Go";
 
+type SearchBarProps = {};
+
 function Searchbar() {
   return (
-    <React.Fragment>
-      <div className="relative">
-        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <GoSearch></GoSearch>
-        </div>
-        <input
-          type="search"
-          id="search"
-          className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Search"
-          required
-        ></input>
+    <div className="relative">
+      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <GoSearch></GoSearch>
       </div>
-    </React.Fragment>
+      <input
+        type="search"
+        id="search"
+        className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Search"
+        required
+      ></input>
+    </div>
   );
 }
 
